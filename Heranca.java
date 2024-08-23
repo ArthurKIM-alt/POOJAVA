3efd4c80e8463aececd058b8adbb32fae9845e74
Tixafacul <luiz.csilva@aluno.faculdadeimpacta.com.br>
2024-08-23 09:42:28 -0300

    Update Heranca.java

--- Heranca.java
+++ Heranca.java
@@ -1,3 +1,44 @@
-public class Heranca {
+class Carro extends Veiculo {
 
+    Carro(String marca, String modelo, int ano, String cor) {
+        super(marca, modelo, ano, cor);
+    }
+
+    void buzinar() {
+        System.out.println("O carro " + marca + " " + modelo + " está buzinando");
+    }
+
+    void acelerar() {
+        System.out.println("O carro " + marca + " " + modelo + " está acelerando.");
+    }
+}
+
+class Moto extends Veiculo {
+
+    Moto(String marca, String modelo, int ano, String cor) {
+        super(marca, modelo, ano, cor);
+    }
+
+    void buzinar() {
+        System.out.println("A moto " + marca + " " + modelo + " está buzinando");
+    }
+
+    void acelerar() {
+        System.out.println("A moto " + marca + " " + modelo + " está acelerando.");
+    }
+}
+
+class Caminhao extends Veiculo {
+
+    Caminhao(String marca, String modelo, int ano, String cor) {
+        super(marca, modelo, ano, cor);
+    }
+
+    void buzinar() {
+        System.out.println("O caminhão " + marca + " " + modelo + " está buzinando");
+    }
+
+    void acelerar() {
+        System.out.println("O caminhão " + marca + " " + modelo + " está acelerando.");
+    }
 }
