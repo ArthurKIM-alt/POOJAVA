e2716dda416f3ff114a0f9a57eda85ab73761528
Tixafacul <luiz.csilva@aluno.faculdadeimpacta.com.br>
2024-08-23 09:40:38 -0300

    Update Abstracao.java

--- Abstracao.java
+++ Abstracao.java
@@ -1,8 +1,16 @@
-abstract class Abstracao {
+abstract class Veiculo {
+
     String marca;
     String modelo;
     int ano;
     String cor;
+
+    Veiculo(String marca, String modelo, int ano, String cor) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.cor = cor;
+    }
 
     abstract void buzinar();
     abstract void acelerar();
