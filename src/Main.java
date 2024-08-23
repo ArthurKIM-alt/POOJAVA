2b57b05cb5d97e3ae669b1b04acc5f0c5e2373d6
Tixafacul <luiz.csilva@aluno.faculdadeimpacta.com.br>
2024-08-23 09:53:48 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Veiculo {
+public class Main {
+    public static void main(String[] args) {
 
-   public String marca;
-   public String modelo;
-   public int ano;
-   public String cor;
+        Veiculo meuCarro = new Carro("Toyota", "Corolla", 2020, "Preto");
+        Veiculo minhaMoto = new Moto("Honda", "CB500", 2021, "Vermelha");
+        Veiculo meuCaminhao = new Caminhao("Scania", "R450", 2019, "Azul");
 
-   public Veiculo(String marca, String modelo, int ano, String cor) {
-       this.marca = marca;
-       this.modelo = modelo;
-       this.ano = ano;
-       this.cor = cor;
-   }
+        meuCarro.buzinar();
+        meuCarro.acelerar();
 
-    public static void main(String[] args)
+        minhaMoto.buzinar();
+        minhaMoto.acelerar();
+
+        meuCaminhao.buzinar();
+        meuCaminhao.acelerar();
+
+        System.out.println("\nCaracterísticas dos veículos:");
+        System.out.println("Carro: " + meuCarro.getMarca() + " " + meuCarro.getModelo() + ", Ano: " + meuCarro.getAno() + ", Cor: " + meuCarro.getCor());
+        System.out.println("Moto: " + minhaMoto.getMarca() + " " + minhaMoto.getModelo() + ", Ano: " + minhaMoto.getAno() + ", Cor: " + minhaMoto.getCor());
+        System.out.println("Caminhão: " + meuCaminhao.getMarca() + " " + meuCaminhao.getModelo() + ", Ano: " + meuCaminhao.getAno() + ", Cor: " + meuCaminhao.getCor());
+    }
 }
